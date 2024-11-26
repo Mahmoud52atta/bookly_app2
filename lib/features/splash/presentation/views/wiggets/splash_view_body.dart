16eb17ba40cd706c7e0eef04579139e518439f32
Mahmoud52atta <github5257@gmail.com@example.com>
2024-11-26 19:58:30 +0200

    Static Ui Splash View Done

---
+++ lib/features/splash/presentation/views/wiggets/splash_view_body.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_app2/core/util/assets.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(Assets.logo),
+        const Text(
+          'Read Free Books',
+          textAlign: TextAlign.center,
+        )
+      ],
+    );
+  }
+}

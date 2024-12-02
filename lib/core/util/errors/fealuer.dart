fa489bde88ccf780795713d0e013811f1ac6af53
Mahmoud52atta <github5257@gmail.com@example.com>
2024-12-02 22:05:03 +0200

    finish Dio exeption

--- lib/core/util/errors/fealuer.dart
+++ lib/core/util/errors/fealuer.dart
@@ -1,3 +1,48 @@
-abstract class Fealuer {}
+import 'package:dio/dio.dart';
 
-class fealuerService extends Fealuer {}
+abstract class Fealuer {
+  final String errorMessage;
+
+  Fealuer({required this.errorMessage});
+}
+
+class ServesFailuer extends Fealuer {
+  ServesFailuer({required super.errorMessage});
+
+  factory ServesFailuer.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServesFailuer(errorMessage: 'Connection timeout with ApiServer');
+      case DioExceptionType.sendTimeout:
+        return ServesFailuer(errorMessage: 'Send timeout with ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServesFailuer(errorMessage: 'Receive timeout with ApiServer');
+      case DioExceptionType.badCertificate:
+        return ServesFailuer(errorMessage: 'BadCertificate with ApiServer');
+      case DioExceptionType.badResponse:
+        return ServesFailuer.fromRespnse(
+            dioError.response!.statusCode!, dioError.response!.data);
+      case DioExceptionType.cancel:
+      // TODO: Handle this case.
+      case DioExceptionType.connectionError:
+        return ServesFailuer(errorMessage: 'Error in Network');
+      case DioExceptionType.unknown:
+        return ServesFailuer(
+            errorMessage: 'Opps tere was an error,please try latter');
+    }
+  }
+  factory ServesFailuer.fromRespnse(int stutuseCode, dynamic response) {
+    if (stutuseCode == 400 || stutuseCode == 401 || stutuseCode == 403) {
+      return ServesFailuer(errorMessage: response['error']['message']);
+    } else if (stutuseCode == 404) {
+      return ServesFailuer(
+          errorMessage: 'Your Request is not found,please try latter');
+    } else if (stutuseCode == 500) {
+      return ServesFailuer(
+          errorMessage: 'Intern Server Error,please try latter');
+    } else {
+      return ServesFailuer(
+          errorMessage: 'Opps tere was an error,please try latter');
+    }
+  }
+}
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app2/core/util/sevices/api_service.dart';
 import 'package:bookly_app2/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app2/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
@@ -20,9 +21,10 @@ class HomeRepoImpl implements HomeRepo {
       }
       return right(books);
     } catch (e) {
-      return left(
-        fealuerService(),
-      );
+      if (e is DioException) {
+        return left(ServesFailuer.fromDioError(e));
+      }
+      return left(ServesFailuer(errorMessage: e.toString()));
     }
   }
 
